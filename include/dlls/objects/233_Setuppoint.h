68010796d78c43f6d5f33e07677cb3c64450a11c
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-26 20:24:38 +0100

    Clean up Setuppoint object

---
+++ include/dlls/objects/233_Setuppoint.h
@@ -0,0 +1,10 @@
+#ifndef DLLS_OBJECTS_233_SETUPPOINT_H_
+#define DLLS_OBJECTS_233_SETUPPOINT_H_
+
+#include "dlls/object_descriptor.h"
+
+void setuppoint_init(void);
+
+extern ObjectDescriptor gSetuppointObjDescriptor;
+
+#endif /* DLLS_OBJECTS_233_SETUPPOINT_H_ */
--- src/dlls/objects/233_Setuppoint/Setuppoint.c
+++ src/dlls/objects/233_Setuppoint/Setuppoint.c
@@ -1,28 +1,26 @@
 /*
- * DLL 0xE9 - Setuppoint.
+ * Setuppoint object (DLL slot 233 / 0xE9).
  *
- * Its only function is setuppoint_init, an empty load-time hook.
+ * This marker object exposes only an empty initialization callback.
  */
-#include "main/dll/dll_00E9_setuppoint.h"
-#include "dlls/object_descriptor.h"
+#include "dlls/objects/233_Setuppoint.h"
 
-void setuppoint_init(void)
-{
+void setuppoint_init(void) {
 }
 
 ObjectDescriptor gSetuppointObjDescriptor = {
-    0,
-    0,
-    0,
-    OBJECT_DESCRIPTOR_FLAGS_10_SLOTS,
-    0,
-    0,
-    0,
-    (ObjectDescriptorCallback)setuppoint_init,
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
+    0,                                         /* reserved0 */
+    0,                                         /* reserved1 */
+    0,                                         /* reserved2 */
+    OBJECT_DESCRIPTOR_FLAGS_10_SLOTS,          /* slotCountAndFlags */
+    0,                                         /* initialise */
+    0,                                         /* release */
+    0,                                         /* slot02 */
+    (ObjectDescriptorCallback)setuppoint_init, /* init */
+    0,                                         /* update */
+    0,                                         /* hitDetect */
+    0,                                         /* render */
+    0,                                         /* free */
+    0,                                         /* getObjectTypeId */
+    0,                                         /* getExtraSize */
 };
