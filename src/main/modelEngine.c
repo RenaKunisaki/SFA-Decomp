dc59b3de9178b3823af2707cbebd8519f401238e
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-27 23:27:49 +0100

    Clean up object DLL 407

--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -201,6 +201,7 @@
 #include "dlls/objects/404_GPSH_Scene.h"
 #include "dlls/objects/405_DBSH_Shrine.h"
 #include "dlls/objects/406_DBSH_Symbol.h"
+#include "dlls/objects/407.h"
 #include "main/audio/sfx_ids.h"
 #include "main/audio/sfx_trigger_ids.h"
 #include "main/audio/sfx.h"
@@ -281,8 +282,7 @@ char lbl_803DB2A0[] = ".";
 #define MODELENGINE_TIMER_DISABLED  2
 #define MODELENGINE_TIMER_RUNNING   4
 
-extern ResourceDescriptor Carryable_funcs, boneParticleEffect_funcs, dll_19, dll_197, dll_199, dll_19A, dll_19B,
-    dll_19C;
+extern ResourceDescriptor Carryable_funcs, boneParticleEffect_funcs, dll_19, dll_199, dll_19A, dll_19B, dll_19C;
 extern ResourceDescriptor dll_19D, dll_19E, dll_1CE, dll_1CF, dll_1D6, dll_1DA, dll_1DB, dll_1FB;
 extern ResourceDescriptor dll_1FF, dll_200, dll_219, dll_21B, dll_224, dll_28B, dll_2A3;
 extern ResourceDescriptor dll_2A4, dll_2E, dll_54, dll_D3, expgfx_funcs;
@@ -1448,7 +1448,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gGPSHSceneObjDescriptor,
     (ResourceDescriptor*)&gDBSHShrineObjDescriptor,
     (ResourceDescriptor*)&gDBSHSymbolObjDescriptor,
-    &dll_197,
+    (ResourceDescriptor*)&gDll407ObjDescriptor,
     &gNWSH_levconObjDescriptor,
     &dll_199,
     &dll_19A,
