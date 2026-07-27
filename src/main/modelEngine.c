676201696760f4575698f69930e6b9cd52b29fe1
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-27 14:41:09 +0100

    Clean up DLL 355 data slot

--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -150,6 +150,7 @@
 #include "dlls/objects/352.h"
 #include "dlls/objects/353_CFTreasRobo.h"
 #include "dlls/objects/354_CFMagicWall.h"
+#include "dlls/objects/355.h"
 #include "main/audio/sfx_ids.h"
 #include "main/audio/sfx_trigger_ids.h"
 #include "main/audio/sfx.h"
@@ -390,7 +391,6 @@ extern ResourceDescriptorCallbacks8 lbl_80320700;
 extern ResourceDescriptor lbl_80323740, lbl_80325928, lbl_80325F20, lbl_80328AD8, lbl_80328E28, lbl_80329340;
 extern ResourceDescriptor lbl_803298D0, Dummy245, Dummy246, Dummy244, Dummy247, Dummy248, Dummy24A, Dummy24B;
 extern ResourceDescriptor lbl_8032A110, lbl_8032AD00, lbl_8032AD68, lbl_8032B6B0;
-extern ResourceDescriptor lbl_803DBEC0;
 extern ResourceDescriptor lbl_803DBEC8, lbl_803DBEE0, lbl_803DC0F8, lbl_803DC100, lbl_803DC108, lbl_803DC138,
     lbl_803DC140, lbl_803DC150;
 extern ResourceDescriptor lbl_803DC158, lbl_803DC2C0, lbl_803DC2D8, lbl_803DC2E0, lbl_803DC2E8, lbl_803DC338,
@@ -1361,7 +1361,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gDll352NullResourceDescriptor,
     (ResourceDescriptor*)&gCFTreasRoboNullResourceDescriptor,
     (ResourceDescriptor*)&gCFMagicWallObjDescriptor,
-    &lbl_803DBEC0,
+    (ResourceDescriptor*)&gDll355NullResourceDescriptor,
     &gCFLevelControlObjDescriptor,
     &lbl_803DBEC8,
     &gExplodedObjDescriptor,
