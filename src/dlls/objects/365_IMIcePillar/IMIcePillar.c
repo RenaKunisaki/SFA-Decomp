cf1688dabed3f8525ae7c0164895f303983801f8
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-28 02:27:25 +0100

    Normalize control flow in IMIcePillar

--- src/dlls/objects/365_IMIcePillar/IMIcePillar.c
+++ src/dlls/objects/365_IMIcePillar/IMIcePillar.c
@@ -3,48 +3,36 @@
 #include "game/objects/object.h"
 #include "main/object_render.h"
 
-int imIcePillar_getExtraSize(void)
-{
+int imIcePillar_getExtraSize(void) {
     return 4;
 }
 
-int imIcePillar_getObjectTypeId(void)
-{
+int imIcePillar_getObjectTypeId(void) {
     return 0;
 }
 
-void imIcePillar_free(void)
-{
+void imIcePillar_free(void) {
 }
 
-void imIcePillar_render(
-    GameObject* obj, int renderArg2, int renderArg3, int renderArg4, int renderArg5, s8 visible)
-{
-    if (visible != 0)
-    {
-        objRenderModelAndHitVolumes(
-            obj, renderArg2, renderArg3, renderArg4, renderArg5, 1.0f);
+void imIcePillar_render(GameObject* obj, int renderArg2, int renderArg3, int renderArg4, int renderArg5, s8 visible) {
+    if (visible != 0) {
+        objRenderModelAndHitVolumes(obj, renderArg2, renderArg3, renderArg4, renderArg5, 1.0f);
     }
 }
 
-void imIcePillar_hitDetect(void)
-{
+void imIcePillar_hitDetect(void) {
 }
 
-void imIcePillar_update(void)
-{
+void imIcePillar_update(void) {
 }
 
-void imIcePillar_init(void)
-{
+void imIcePillar_init(void) {
 }
 
-void imIcePillar_release(void)
-{
+void imIcePillar_release(void) {
 }
 
-void imIcePillar_initialise(void)
-{
+void imIcePillar_initialise(void) {
 }
 
 ObjectDescriptor gIMIcePillarObjDescriptor = {
