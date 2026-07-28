509b913ea7a908280560baea6d90e2060839a136
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-28 19:08:35 +0100

    Clean up object DLL slot 520

--- src/dlls/objects/520_WM_Wallpowe/WM_Wallpowe.c
+++ src/dlls/objects/520_WM_Wallpowe/WM_Wallpowe.c
@@ -1,4 +1,4 @@
-/* WM_Wallpowe (DLL 0x0208) */
-#include "global.h"
+#include "dlls/objects/520_WM_Wallpowe.h"
 
-u32 lbl_803DC108[2] = {0};
+/* Functionless DLL 0x0208 owns this two-word null resource record. */
+u32 gWM_WallpoweNullResourceDescriptor[WM_WALLPOWE_NULL_DESCRIPTOR_WORD_COUNT] = {0};
--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -313,6 +313,7 @@
 #include "dlls/objects/517_WM_Vein.h"
 #include "dlls/objects/518_LightSource.h"
 #include "dlls/objects/519_WM_Worm.h"
+#include "dlls/objects/520_WM_Wallpowe.h"
 #include "main/audio/sfx_ids.h"
 #include "main/audio/sfx_trigger_ids.h"
 #include "main/audio/sfx.h"
@@ -505,7 +506,7 @@ extern ResourceDescriptorCallbacks8 lbl_80320700;
 extern ResourceDescriptor lbl_80328E28, lbl_80329340;
 extern ResourceDescriptor gDll22CObjDescriptor, Dummy245, Dummy246, Dummy244, Dummy247, Dummy248, Dummy24A, Dummy24B;
 extern ResourceDescriptor lbl_8032A110, lbl_8032AD00, lbl_8032AD68, gDll29EObjDescriptor;
-extern ResourceDescriptor lbl_803DC108, lbl_803DC138, lbl_803DC140, lbl_803DC150;
+extern ResourceDescriptor lbl_803DC138, lbl_803DC140, lbl_803DC150;
 extern ResourceDescriptor lbl_803DC158, lbl_803DC2C0, lbl_803DC2D8, lbl_803DC2E0, lbl_803DC2E8, lbl_803DC338,
     lbl_803DC358, lbl_803DC360;
 extern ResourceDescriptor lbl_803DC368, lbl_803DC370, lbl_803DC378, lbl_803DC388, lbl_803DC390, lbl_803DC6E8,
@@ -1639,7 +1640,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gWMVeinNullResourceDescriptor,
     (ResourceDescriptor*)&gLightSourceObjDescriptor,
     (ResourceDescriptor*)&gWM_WormObjDescriptor,
-    &lbl_803DC108,
+    (ResourceDescriptor*)&gWM_WallpoweNullResourceDescriptor,
     &gWM_LevelControlObjDescriptor,
     &gWM_GeneralScalesObjDescriptor,
     &gFireFlyObjDescriptor,
