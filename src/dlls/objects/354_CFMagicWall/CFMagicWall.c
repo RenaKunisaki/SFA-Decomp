f0d8aedc057b9801cd97811fd36a45eb991fdbc6
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-28 02:16:37 +0100

    Normalize control flow in CF magic wall

--- src/dlls/objects/354_CFMagicWall/CFMagicWall.c
+++ src/dlls/objects/354_CFMagicWall/CFMagicWall.c
@@ -24,8 +24,9 @@ void cfmagicwall_free(void) {
 
 void cfmagicwall_render(GameObject* obj, int renderArg2, int renderArg3, int renderArg4, int renderArg5, s8 visible) {
     s32 v = visible;
-    if (v != 0)
+    if (v != 0) {
         objRenderModelAndHitVolumes(obj, renderArg2, renderArg3, renderArg4, renderArg5, 1.0f);
+    }
 }
 
 void cfmagicwall_hitDetect(void) {
