6dd0553723d7ca9477c7f08596c3aae71424924f
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-28 14:59:12 +0100

    Clean up object DLL slot 514

--- src/dlls/objects/514/514.c
+++ src/dlls/objects/514/514.c
@@ -1,4 +1,5 @@
-/* DLL 0x0202 */
-#include "global.h"
+/* Empty DLL 0x202 resource slot. */
 
-u32 lbl_803DC0F8[2] = {0};
+#include "dlls/objects/514.h"
+
+u32 gDll202NullResourceDescriptor[DLL202_NULL_DESCRIPTOR_WORD_COUNT] = {0};
