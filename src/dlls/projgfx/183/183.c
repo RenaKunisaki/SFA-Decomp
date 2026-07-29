194e8f3c2257c9b03e134bf100de8d9b5f8c65da
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 13:02:34 +0100

    Clean up projship1 DLL 183

--- src/dlls/projgfx/183/183.c
+++ src/dlls/projgfx/183/183.c
@@ -6,28 +6,28 @@
  * message and returns -1. release/initialise are empty lifecycle hooks
  * kept so the object descriptor / DLL loader still resolves.
  */
-#include "dolphin/os/OSReport.h"
-#include "main/dll/dll_descriptor_table.h"
 #include "main/dll/dll_00B7_projship1.h"
+#include "dolphin/os/OSReport.h"
 
-#define PROJECTILE_UNSUPPORTED_RETURN -1
+extern char sProjship1DoNoLongerSupported[];
 
-int projship1_doUnsupported(void)
-{
+int projship1_doUnsupported(void) {
     OSReport(sProjship1DoNoLongerSupported);
-    return PROJECTILE_UNSUPPORTED_RETURN;
+    return -1;
 }
 
-void projship1_release(void)
-{
+void projship1_release(void) {
 }
 
-void projship1_initialise(void)
-{
+void projship1_initialise(void) {
 }
 
-DllDescriptorTable lbl_803197F8 = {{(void*)0x00000000, (void*)0x00000000, (void*)0x00000000, (void*)0x00030000,
-                                    projship1_initialise, projship1_release, (void*)0x00000000,
-                                    projship1_doUnsupported}};
+Projship1ResourceDescriptor gProjship1ResourceDescriptor = {
+    {0x00000000, 0x00000000, 0x00000000, 0x00030000},
+    projship1_initialise,
+    projship1_release,
+    NULL,
+    projship1_doUnsupported,
+};
 
 char sProjship1DoNoLongerSupported[] = "<projship1 Do>No Longer supported \n";
--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -437,6 +437,7 @@
 #include "main/dll/dll_00B4_projenergise1.h"
 #include "main/dll/dll_00B5_projenergise2.h"
 #include "main/dll/dll_00B6_projsquirt1.h"
+#include "main/dll/dll_00B7_projship1.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
 #include "main/dll/dll_0282_barrelgener.h"
@@ -584,8 +585,8 @@ extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
 extern ResourceDescriptor lbl_803195E8, lbl_80319638, lbl_80319688;
-extern ResourceDescriptor lbl_803197F8, lbl_80319840, projquakeshock_funcs, lbl_803198D8, projtesla_funcs, lbl_80319968,
-    lbl_803199B0, lbl_803199F8;
+extern ResourceDescriptor lbl_80319840, projquakeshock_funcs, lbl_803198D8, projtesla_funcs, lbl_80319968, lbl_803199B0,
+    lbl_803199F8;
 extern ResourceDescriptor lbl_80319A40;
 extern ResourceDescriptor lbl_8031A148;
 extern ResourceDescriptor TitleScreenInit_funcs, n_rareware_funcs, n_attractmode_funcs, lbl_8031A82C, EnterSaveNameScreen_funcs, OptionsScreen_funcs,
@@ -1398,7 +1399,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gProjenergise1ResourceDescriptor,
     (ResourceDescriptor*)&gProjenergise2ResourceDescriptor,
     (ResourceDescriptor*)&gProjsquirt1ResourceDescriptor,
-    &lbl_803197F8,
+    (ResourceDescriptor*)&gProjship1ResourceDescriptor,
     &lbl_803195E8,
     &lbl_80319638,
     &lbl_80319688,
