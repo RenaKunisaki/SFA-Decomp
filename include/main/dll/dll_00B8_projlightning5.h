4edfdfe3d902c7e2f4d967188c75fd1102d03489
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 13:03:52 +0100

    Clean up projlightning5 DLL 184

--- include/main/dll/dll_00B8_projlightning5.h
+++ include/main/dll/dll_00B8_projlightning5.h
@@ -1,7 +1,27 @@
 #ifndef MAIN_DLL_DLL_00B8_PROJLIGHTNING5_H_
 #define MAIN_DLL_DLL_00B8_PROJLIGHTNING5_H_
 
-extern char sProjlightning5DoNoLongerSupported[];
+#include "global.h"
+#include "main/resource.h"
+
+typedef int (*Projlightning5UnsupportedFn)(void);
+
+typedef struct Projlightning5ResourceDescriptor {
+    u32 metadata[4];
+    ResourceDescriptorCallback initialise;
+    ResourceDescriptorCallback release;
+    ResourceDescriptorCallback reserved18;
+    Projlightning5UnsupportedFn unsupported;
+} Projlightning5ResourceDescriptor;
+
+STATIC_ASSERT(offsetof(Projlightning5ResourceDescriptor, metadata) == 0x00);
+STATIC_ASSERT(offsetof(Projlightning5ResourceDescriptor, initialise) == 0x10);
+STATIC_ASSERT(offsetof(Projlightning5ResourceDescriptor, release) == 0x14);
+STATIC_ASSERT(offsetof(Projlightning5ResourceDescriptor, reserved18) == 0x18);
+STATIC_ASSERT(offsetof(Projlightning5ResourceDescriptor, unsupported) == 0x1C);
+STATIC_ASSERT(sizeof(Projlightning5ResourceDescriptor) == 0x20);
+
+extern Projlightning5ResourceDescriptor gProjlightning5ResourceDescriptor;
 
 int projlightning5_doUnsupported(void);
 void projlightning5_release(void);
--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -438,6 +438,7 @@
 #include "main/dll/dll_00B5_projenergise2.h"
 #include "main/dll/dll_00B6_projsquirt1.h"
 #include "main/dll/dll_00B7_projship1.h"
+#include "main/dll/dll_00B8_projlightning5.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
 #include "main/dll/dll_0282_barrelgener.h"
@@ -584,7 +585,7 @@ extern ResourceDescriptor Effect9_funcs, Effect10_funcs, Effect11_funcs, Effect1
 extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect20_funcs, lbl_803112E8, lbl_80311340,
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
-extern ResourceDescriptor lbl_803195E8, lbl_80319638, lbl_80319688;
+extern ResourceDescriptor lbl_80319638, lbl_80319688;
 extern ResourceDescriptor lbl_80319840, projquakeshock_funcs, lbl_803198D8, projtesla_funcs, lbl_80319968, lbl_803199B0,
     lbl_803199F8;
 extern ResourceDescriptor lbl_80319A40;
@@ -1400,7 +1401,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gProjenergise2ResourceDescriptor,
     (ResourceDescriptor*)&gProjsquirt1ResourceDescriptor,
     (ResourceDescriptor*)&gProjship1ResourceDescriptor,
-    &lbl_803195E8,
+    (ResourceDescriptor*)&gProjlightning5ResourceDescriptor,
     &lbl_80319638,
     &lbl_80319688,
     &lbl_80319840,
