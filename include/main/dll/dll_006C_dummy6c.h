25023db622bfb709e558136367dc5d1dacb5244b
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 07:24:31 +0100

    Recover Dummy6C resource contract

--- include/main/dll/dll_006C_dummy6c.h
+++ include/main/dll/dll_006C_dummy6c.h
@@ -1,7 +1,27 @@
 #ifndef MAIN_DLL_DLL_006C_DUMMY6C_H_
 #define MAIN_DLL_DLL_006C_DUMMY6C_H_
 
-int Dummy6C_func03_ret_0(void);
+#include "global.h"
+#include "main/resource.h"
+
+typedef struct Dummy6CDescriptor {
+    u32 metadata[4];
+    void (*initialise)(void);
+    void (*release)(void);
+    ResourceDescriptorCallback reserved18;
+    int (*init)(void);
+} Dummy6CDescriptor;
+
+STATIC_ASSERT(offsetof(Dummy6CDescriptor, metadata) == 0x00);
+STATIC_ASSERT(offsetof(Dummy6CDescriptor, initialise) == 0x10);
+STATIC_ASSERT(offsetof(Dummy6CDescriptor, release) == 0x14);
+STATIC_ASSERT(offsetof(Dummy6CDescriptor, reserved18) == 0x18);
+STATIC_ASSERT(offsetof(Dummy6CDescriptor, init) == 0x1C);
+STATIC_ASSERT(sizeof(Dummy6CDescriptor) == 0x20);
+
+extern Dummy6CDescriptor gDummy6CDescriptor;
+
+int Dummy6C_init(void);
 void Dummy6C_release(void);
 void Dummy6C_initialise(void);
 
