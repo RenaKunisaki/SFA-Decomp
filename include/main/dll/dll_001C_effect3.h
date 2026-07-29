f8b46a090810a017c4fa646df3092e2ff4d14082
Zachary Canann <zacharycanann@gmail.com>
2026-07-29 16:14:48 -0700

    effect3: type attached source objects

--- include/main/dll/dll_001C_effect3.h
+++ include/main/dll/dll_001C_effect3.h
@@ -1,13 +1,14 @@
 #ifndef MAIN_DLL_DLL_001C_EFFECT3_H_
 #define MAIN_DLL_DLL_001C_EFFECT3_H_
 
+#include "game/objects/object_fwd.h"
 #include "main/dll/partfx_interface.h"
 
 void Effect3_func05_nop(void);
 void Effect3_func03_nop(void);
 void Effect3_release(void);
 void Effect3_initialise(void);
-int Effect3_spawnEffect(s16* sourceObj, int effectId, PartFxSpawnParams* spawnParamsIn, u32 spawnFlags, u8 modelId,
-                        f32* extraArgs);
+int Effect3_spawnEffect(GameObject* sourceObject, int effectId, PartFxSpawnParams* spawnParamsIn, u32 spawnFlags,
+                        u8 modelId, f32* extraArgs);
 
 #endif /* MAIN_DLL_DLL_001C_EFFECT3_H_ */
