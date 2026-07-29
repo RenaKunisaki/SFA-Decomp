a8b69096ca3fa7c60acaee37c18624cfb32da29c
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 12:58:57 +0100

    Clean up projenergise1 DLL 180

--- src/dlls/projgfx/180/180.c
+++ src/dlls/projgfx/180/180.c
@@ -7,28 +7,28 @@
  * placeholder that keeps the DLL id valid after the projectile behavior
  * was removed.
  */
-#include "dolphin/os/OSReport.h"
-#include "main/dll/dll_descriptor_table.h"
 #include "main/dll/dll_00B4_projenergise1.h"
+#include "dolphin/os/OSReport.h"
 
-#define PROJECTILE_UNSUPPORTED_RETURN -1
+extern char sProjenergise1DoNoLongerSupported[];
 
-int projenergise1_doUnsupported(void)
-{
+int projenergise1_doUnsupported(void) {
     OSReport(sProjenergise1DoNoLongerSupported);
-    return PROJECTILE_UNSUPPORTED_RETURN;
+    return -1;
 }
 
-void projenergise1_release(void)
-{
+void projenergise1_release(void) {
 }
 
-void projenergise1_initialise(void)
-{
+void projenergise1_initialise(void) {
 }
 
-DllDescriptorTable lbl_803196D8 = {{(void*)0x00000000, (void*)0x00000000, (void*)0x00000000, (void*)0x00030000,
-                                    projenergise1_initialise, projenergise1_release, (void*)0x00000000,
-                                    projenergise1_doUnsupported}};
+Projenergise1ResourceDescriptor gProjenergise1ResourceDescriptor = {
+    {0x00000000, 0x00000000, 0x00000000, 0x00030000},
+    projenergise1_initialise,
+    projenergise1_release,
+    NULL,
+    projenergise1_doUnsupported,
+};
 
 char sProjenergise1DoNoLongerSupported[] = "<projenergise1 Do>No Longer supported \n";
--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -434,6 +434,7 @@
 #include "main/dll/dll_00B1_projlightning3.h"
 #include "main/dll/dll_00B2_projrobotfire.h"
 #include "main/dll/dll_00B3_projlightning4.h"
+#include "main/dll/dll_00B4_projenergise1.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
 #include "main/dll/dll_0282_barrelgener.h"
@@ -580,7 +581,7 @@ extern ResourceDescriptor Effect9_funcs, Effect10_funcs, Effect11_funcs, Effect1
 extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect20_funcs, lbl_803112E8, lbl_80311340,
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
-extern ResourceDescriptor lbl_803195E8, lbl_80319638, lbl_80319688, lbl_803196D8, projenergise2_funcs, lbl_803197B0;
+extern ResourceDescriptor lbl_803195E8, lbl_80319638, lbl_80319688, projenergise2_funcs, lbl_803197B0;
 extern ResourceDescriptor lbl_803197F8, lbl_80319840, projquakeshock_funcs, lbl_803198D8, projtesla_funcs, lbl_80319968,
     lbl_803199B0, lbl_803199F8;
 extern ResourceDescriptor lbl_80319A40;
@@ -1392,7 +1393,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gProjlightning3ResourceDescriptor,
     (ResourceDescriptor*)&gProjrobotfireResourceDescriptor,
     (ResourceDescriptor*)&gProjlightning4ResourceDescriptor,
-    &lbl_803196D8,
+    (ResourceDescriptor*)&gProjenergise1ResourceDescriptor,
     &projenergise2_funcs,
     &lbl_803197B0,
     &lbl_803197F8,
