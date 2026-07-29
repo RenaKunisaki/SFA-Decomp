bfe2505dbaa1637da478cf562ba0da9288b841c6
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 10:19:42 +0100

    Recover DLL 8C layered effect contract

--- include/main/dll/foodbag.h
+++ include/main/dll/foodbag.h
@@ -3,7 +3,4 @@
 
 #include "ghidra_import.h"
 
-void dll_8C_func00_nop(void);
-void dll_8C_func01_nop(void);
-void dll_8C_func03(int param_1,int param_2,int param_3,u32 param_4);
 #endif /* MAIN_DLL_FOODBAG_H_ */
