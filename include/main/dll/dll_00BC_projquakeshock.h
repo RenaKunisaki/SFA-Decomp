904fe5273d0f31eb5d58fb77d434abf881c13bfd
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 13:09:08 +0100

    Canonicalise projquakeshock resource descriptor

--- include/main/dll/dll_00BC_projquakeshock.h
+++ include/main/dll/dll_00BC_projquakeshock.h
@@ -1,7 +1,27 @@
 #ifndef MAIN_DLL_DLL_00BC_PROJQUAKESHOCK_H_
 #define MAIN_DLL_DLL_00BC_PROJQUAKESHOCK_H_
 
-extern char sProjquakeshockDoNoLongerSupported[];
+#include "global.h"
+#include "main/resource.h"
+
+typedef int (*ProjquakeshockUnsupportedFn)(void);
+
+typedef struct ProjquakeshockResourceDescriptor {
+    u32 metadata[4];
+    ResourceDescriptorCallback initialise;
+    ResourceDescriptorCallback release;
+    ResourceDescriptorCallback reserved18;
+    ProjquakeshockUnsupportedFn unsupported;
+} ProjquakeshockResourceDescriptor;
+
+STATIC_ASSERT(offsetof(ProjquakeshockResourceDescriptor, metadata) == 0x00);
+STATIC_ASSERT(offsetof(ProjquakeshockResourceDescriptor, initialise) == 0x10);
+STATIC_ASSERT(offsetof(ProjquakeshockResourceDescriptor, release) == 0x14);
+STATIC_ASSERT(offsetof(ProjquakeshockResourceDescriptor, reserved18) == 0x18);
+STATIC_ASSERT(offsetof(ProjquakeshockResourceDescriptor, unsupported) == 0x1C);
+STATIC_ASSERT(sizeof(ProjquakeshockResourceDescriptor) == 0x20);
+
+extern ProjquakeshockResourceDescriptor gProjquakeshockResourceDescriptor;
 
 int projquakeshock_doUnsupported(void);
 void projquakeshock_release(void);
--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -442,6 +442,7 @@
 #include "main/dll/dll_00B9_projlightning7.h"
 #include "main/dll/dll_00BA_projlightning6.h"
 #include "main/dll/dll_00BB_projwallpower.h"
+#include "main/dll/dll_00BC_projquakeshock.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
 #include "main/dll/dll_0282_barrelgener.h"
@@ -588,7 +589,7 @@ extern ResourceDescriptor Effect9_funcs, Effect10_funcs, Effect11_funcs, Effect1
 extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect20_funcs, lbl_803112E8, lbl_80311340,
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
-extern ResourceDescriptor projquakeshock_funcs, lbl_803198D8, projtesla_funcs, lbl_80319968, lbl_803199B0, lbl_803199F8;
+extern ResourceDescriptor lbl_803198D8, projtesla_funcs, lbl_80319968, lbl_803199B0, lbl_803199F8;
 extern ResourceDescriptor lbl_80319A40;
 extern ResourceDescriptor lbl_8031A148;
 extern ResourceDescriptor TitleScreenInit_funcs, n_rareware_funcs, n_attractmode_funcs, lbl_8031A82C, EnterSaveNameScreen_funcs, OptionsScreen_funcs,
@@ -1406,7 +1407,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gProjlightning7ResourceDescriptor,
     (ResourceDescriptor*)&gProjlightning6ResourceDescriptor,
     (ResourceDescriptor*)&gProjwallpowerResourceDescriptor,
-    &projquakeshock_funcs,
+    (ResourceDescriptor*)&gProjquakeshockResourceDescriptor,
     &lbl_803198D8,
     &projtesla_funcs,
     &lbl_80319968,
