cccd1776889028ebd6dca6ff0dc5892d4bba4215
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 22:47:15 +0100

    Canonicalise projcore3 resource descriptor

--- src/dlls/projgfx/193/193.c
+++ src/dlls/projgfx/193/193.c
@@ -6,26 +6,28 @@
  * message and reports failure (-1). release/initialise are empty no-ops.
  * The DLL is kept as a placeholder so its slot/id remains valid.
  */
-#include "dolphin/os.h"
-#include "main/dll/dll_descriptor_table.h"
 #include "main/dll/dll_00C1_projcore3.h"
+#include "dolphin/os/OSReport.h"
 
-int projcore3_doUnsupported(void)
-{
+extern char sProjcore3DoNoLongerSupported[];
+
+int projcore3_doUnsupported(void) {
     OSReport(sProjcore3DoNoLongerSupported);
     return -1;
 }
 
-void projcore3_release(void)
-{
+void projcore3_release(void) {
 }
 
-void projcore3_initialise(void)
-{
+void projcore3_initialise(void) {
 }
 
-DllDescriptorTable lbl_803199F8 = {{(void*)0x00000000, (void*)0x00000000, (void*)0x00000000, (void*)0x00030000,
-                                    projcore3_initialise, projcore3_release, (void*)0x00000000,
-                                    projcore3_doUnsupported}};
+Projcore3ResourceDescriptor gProjcore3ResourceDescriptor = {
+    {0x00000000, 0x00000000, 0x00000000, 0x00030000},
+    projcore3_initialise,
+    projcore3_release,
+    NULL,
+    projcore3_doUnsupported,
+};
 
 char sProjcore3DoNoLongerSupported[] = "<projcore3 Do>No Longer supported \n";
