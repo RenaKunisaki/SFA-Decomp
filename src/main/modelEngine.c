822365b7571ef42a293010df75c6929c12ea49ba
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 10:25:32 +0100

    Recover DLL 8D variant effect contract

--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -395,6 +395,7 @@
 #include "main/dll/dll_008A_modgfx.h"
 #include "main/dll/dll_008B_modgfx.h"
 #include "main/dll/dll_008C_modgfx.h"
+#include "main/dll/dll_008D_modgfx.h"
 #include "main/dll/dll_00A3_modgfx.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
@@ -542,7 +543,7 @@ extern ResourceDescriptor Effect9_funcs, Effect10_funcs, Effect11_funcs, Effect1
 extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect20_funcs, lbl_803112E8, lbl_80311340,
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
-extern ResourceDescriptor dll_8D_funcs, dll_8E_funcs,
+extern ResourceDescriptor dll_8E_funcs,
     dll_8F_funcs, dll_90_funcs;
 extern ResourceDescriptor dll_91_funcs, dll_92_funcs, dll_93_funcs, dll_94_funcs, dll_95_funcs, dll_96_funcs,
     dll_97_funcs, dll_98_funcs;
@@ -1326,7 +1327,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gDll8AResourceDescriptor,
     (ResourceDescriptor*)&gDll8BResourceDescriptor,
     (ResourceDescriptor*)&gDll8CResourceDescriptor,
-    &dll_8D_funcs,
+    (ResourceDescriptor*)&gDll8DResourceDescriptor,
     &dll_8E_funcs,
     &dll_8F_funcs,
     &dll_90_funcs,
