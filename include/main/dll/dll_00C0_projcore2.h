fe0209018ffeece0c205bc5154a6263de337491e
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 22:46:05 +0100

    Clean up projcore2 DLL 192

--- include/main/dll/dll_00C0_projcore2.h
+++ include/main/dll/dll_00C0_projcore2.h
@@ -1,7 +1,27 @@
 #ifndef MAIN_DLL_DLL_00C0_PROJCORE2_H_
 #define MAIN_DLL_DLL_00C0_PROJCORE2_H_
 
-extern char sProjcore2DoNoLongerSupported[];
+#include "global.h"
+#include "main/resource.h"
+
+typedef int (*Projcore2UnsupportedFn)(void);
+
+typedef struct Projcore2ResourceDescriptor {
+    u32 metadata[4];
+    ResourceDescriptorCallback initialise;
+    ResourceDescriptorCallback release;
+    ResourceDescriptorCallback reserved18;
+    Projcore2UnsupportedFn unsupported;
+} Projcore2ResourceDescriptor;
+
+STATIC_ASSERT(offsetof(Projcore2ResourceDescriptor, metadata) == 0x00);
+STATIC_ASSERT(offsetof(Projcore2ResourceDescriptor, initialise) == 0x10);
+STATIC_ASSERT(offsetof(Projcore2ResourceDescriptor, release) == 0x14);
+STATIC_ASSERT(offsetof(Projcore2ResourceDescriptor, reserved18) == 0x18);
+STATIC_ASSERT(offsetof(Projcore2ResourceDescriptor, unsupported) == 0x1C);
+STATIC_ASSERT(sizeof(Projcore2ResourceDescriptor) == 0x20);
+
+extern Projcore2ResourceDescriptor gProjcore2ResourceDescriptor;
 
 int projcore2_doUnsupported(void);
 void projcore2_release(void);
