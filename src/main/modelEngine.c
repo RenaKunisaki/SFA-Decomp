0626ed170024d9b0b857aacac78c4d16f3ad6e6a
Jack Price-Burns <jackpriceburns@outlook.com>
2026-07-29 09:11:54 +0100

    Recover DLL 7E billboard effect contract

--- src/main/modelEngine.c
+++ src/main/modelEngine.c
@@ -380,6 +380,7 @@
 #include "main/dll/dll_007B_modgfx.h"
 #include "main/dll/dll_007C_modgfx.h"
 #include "main/dll/dll_007D_modgfx.h"
+#include "main/dll/dll_007E_modgfx.h"
 #include "main/dll/dll_00A3_modgfx.h"
 #include "main/dll/dll_0000_gameui_api.h"
 #include "main/dll/dll_00DA_pollenfragment_api.h"
@@ -527,8 +528,7 @@ extern ResourceDescriptor Effect9_funcs, Effect10_funcs, Effect11_funcs, Effect1
 extern ResourceDescriptor Effect17_funcs, Effect18_funcs, Effect19_funcs, Effect20_funcs, lbl_803112E8, lbl_80311340,
     lbl_80311378, lbl_80311438;
 extern ResourceDescriptor lbl_803114B0, lbl_803114D8, lbl_803115F8, dll_15_funcs, lbl_80311900, lbl_80311BE0;
-extern ResourceDescriptor dll_7E_funcs,
-    dll_7F_funcs, dll_80_funcs;
+extern ResourceDescriptor dll_7F_funcs, dll_80_funcs;
 extern ResourceDescriptor dll_81_funcs, dll_82_funcs, dll_83_funcs, dll_84_funcs, dll_85_funcs, dll_86_funcs,
     dll_87_funcs, dll_88_funcs;
 extern ResourceDescriptor dll_89_funcs, dll_8A_funcs, dll_8B_funcs, dll_8C_funcs, dll_8D_funcs, dll_8E_funcs,
@@ -1300,7 +1300,7 @@ ResourceDescriptor* gResourceDescriptors[] = {
     (ResourceDescriptor*)&gDll7BResourceDescriptor,
     (ResourceDescriptor*)&gDll7CResourceDescriptor,
     (ResourceDescriptor*)&gDll7DResourceDescriptor,
-    &dll_7E_funcs,
+    (ResourceDescriptor*)&gDll7EResourceDescriptor,
     &dll_7F_funcs,
     &dll_80_funcs,
     &dll_81_funcs,
