3d39ef72a01b76c567abedba04f0f4cf8a5d9310
Zachary Canann <zacharycanann@gmail.com>
2026-07-31 22:48:33 -0700

    engine/60: remove address-split phantom declarations

--- include/main/dll/baddie/dll_003C_TumbleweedBush.h
+++ include/main/dll/baddie/dll_003C_TumbleweedBush.h
@@ -9,24 +9,5 @@ void setLinkIsRotated(void);
 u16 linkGetSelectedItemId(void);
 void Link_refreshOverlappingItemTimers(void);
 void Link_scanItemVerticalBounds(void);
-void FUN_801310fc(u64 param_1,double param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 u32 param_9,u32 param_10,u8 param_11,u8 *param_12,
-                 u32 param_13,u32 param_14,u32 param_15,u32 param_16,
-                 u16 param_17,u16 param_18,u16 param_19,u16 param_20);
-void FUN_80131140(u64 param_1,double param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 u32 param_9,u32 param_10,u32 param_11,u32 param_12,
-                 u32 param_13,u32 param_14,u32 param_15,u32 param_16);
-void FUN_80131348(u64 param_1,u64 param_2,u64 param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 short *param_9,u32 param_10,u32 param_11,u32 param_12,
-                 u32 param_13,u32 param_14,u32 param_15,u32 param_16);
-void FUN_80131c1c(u32 param_1,u32 param_2,short param_3,short param_4,short param_5,
-                 u16 param_6);
-void FUN_80131cc4(u64 param_1,double param_2,double param_3,u64 param_4,
-                 u64 param_5,u64 param_6,u64 param_7,u64 param_8,
-                 u32 param_9,u32 param_10,u32 param_11,u32 param_12,
-                 u32 param_13,u32 param_14,u32 param_15,u32 param_16);
 
 #endif /* MAIN_DLL_BADDIE_TUMBLEWEEDBUSH_H_ */
